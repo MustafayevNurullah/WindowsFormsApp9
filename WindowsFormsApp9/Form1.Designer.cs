b5f7a63ef99963e3f15350c4386b4491a06f9035
nurlan3295 <nurlan3295@gmail.com>
2019-01-15 21:51:13 +0400

    Add project files.

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Font = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Size = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.B = new System.Windows.Forms.CheckBox();
+            this.U = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.L = new System.Windows.Forms.CheckBox();
+            this.C = new System.Windows.Forms.CheckBox();
+            this.R = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Font
+            // 
+            this.Font.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Font.FormattingEnabled = true;
+            this.Font.Location = new System.Drawing.Point(12, 41);
+            this.Font.Name = "Font";
+            this.Font.Size = new System.Drawing.Size(71, 21);
+            this.Font.TabIndex = 0;
+            this.Font.SelectedIndexChanged += new System.EventHandler(this.Font_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Font";
+            // 
+            // Size
+            // 
+            this.Size.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Size.FormattingEnabled = true;
+            this.Size.Location = new System.Drawing.Point(121, 41);
+            this.Size.Name = "Size";
+            this.Size.Size = new System.Drawing.Size(47, 21);
+            this.Size.TabIndex = 2;
+            this.Size.SelectedIndexChanged += new System.EventHandler(this.Size_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Size";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "FrontStyle";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(354, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Alignment";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(500, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Color";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(588, 8);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(588, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(713, 8);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "Load";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // B
+            // 
+            this.B.Appearance = System.Windows.Forms.Appearance.Button;
+            this.B.AutoSize = true;
+            this.B.Location = new System.Drawing.Point(204, 45);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(24, 23);
+            this.B.TabIndex = 18;
+            this.B.Text = "B";
+            this.B.UseVisualStyleBackColor = true;
+            // 
+            // U
+            // 
+            this.U.Appearance = System.Windows.Forms.Appearance.Button;
+            this.U.AutoSize = true;
+            this.U.Location = new System.Drawing.Point(246, 45);
+            this.U.Name = "U";
+            this.U.Size = new System.Drawing.Size(25, 23);
+            this.U.TabIndex = 19;
+            this.U.Text = "U";
+            this.U.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(286, 44);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(22, 23);
+            this.checkBox3.TabIndex = 20;
+            this.checkBox3.Text = "/";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // L
+            // 
+            this.L.Appearance = System.Windows.Forms.Appearance.Button;
+            this.L.AutoSize = true;
+            this.L.Location = new System.Drawing.Point(341, 43);
+            this.L.Name = "L";
+            this.L.Size = new System.Drawing.Size(23, 23);
+            this.L.TabIndex = 21;
+            this.L.Text = "L";
+            this.L.UseVisualStyleBackColor = true;
+            this.L.CheckedChanged += new System.EventHandler(this.L_CheckedChanged);
+            // 
+            // C
+            // 
+            this.C.Appearance = System.Windows.Forms.Appearance.Button;
+            this.C.AutoSize = true;
+            this.C.Location = new System.Drawing.Point(379, 42);
+            this.C.Name = "C";
+            this.C.Size = new System.Drawing.Size(24, 23);
+            this.C.TabIndex = 22;
+            this.C.Text = "C";
+            this.C.UseVisualStyleBackColor = true;
+            this.C.CheckedChanged += new System.EventHandler(this.C_CheckedChanged);
+            // 
+            // R
+            // 
+            this.R.Appearance = System.Windows.Forms.Appearance.Button;
+            this.R.AutoSize = true;
+            this.R.Location = new System.Drawing.Point(418, 42);
+            this.R.Name = "R";
+            this.R.Size = new System.Drawing.Size(25, 23);
+            this.R.TabIndex = 23;
+            this.R.Text = "R";
+            this.R.UseVisualStyleBackColor = true;
+            this.R.CheckedChanged += new System.EventHandler(this.R_CheckedChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(-10, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(808, 378);
+            this.richTextBox1.TabIndex = 24;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(479, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.R);
+            this.Controls.Add(this.C);
+            this.Controls.Add(this.L);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.U);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Size);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Font);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Font;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox Size;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox B;
+        private System.Windows.Forms.CheckBox U;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox L;
+        private System.Windows.Forms.CheckBox C;
+        private System.Windows.Forms.CheckBox R;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 8; i < 75; i+=5)
+                NewMethod(i);
+            // Font.Items.AddRange(FontFamily.Families);
+            List<string> fonts = new List<string>();
+
+            foreach (FontFamily font in System.Drawing.FontFamily.Families)
+            {
+                Font.Items.Add(font.Name);
+            }
+
+            richTextBox1.Font = new Font("Calibri", 11);
+        }
+
+        private void NewMethod(int i)
+        {
+            Size.Items.Add(i);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.BackColor = colorDialog.Color;
+            }
+
+        }
+
+        private void L_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+        }
+
+        private void C_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Center;
+        }
+
+        private void R_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+        }
+
+        private void Size_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (Size.SelectedIndex)
+            {
+                case 0:
+                    richTextBox1.Font = new Font(Font.SelectedIndex + 1.ToString(), 8);
+
+                    break;
+                case 1:
+                    richTextBox1.Font = new Font(Font.SelectedIndex + 1.ToString(), 13);
+
+                    break;
+                case 2:
+                    richTextBox1.Font = new Font(Font.SelectedIndex + 1.ToString(), 18);
+
+                    break;
+                case 3:
+                    richTextBox1.Font = new Font(Font.SelectedIndex + 1.ToString(), 23);
+
+                    break;
+
+
+            }
+
+
+            richTextBox1.Font = new Font(Font.SelectedIndex+1.ToString(), Size.SelectedIndex+1);
+           
+        }
+
+        private void Font_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var a = Font.SelectedIndex;
+      
+            foreach (FontFamily font in System.Drawing.FontFamily.Families)
+            {
+                if(a==Font.SelectedIndex+1)
+                {
+                   
+            richTextBox1.Font = new Font(font.Name, Size.SelectedIndex+1);
+                }
+            }
+
+        }
+    }
+}
